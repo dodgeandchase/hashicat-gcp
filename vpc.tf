d18e468491ee5738f0651655108d903cb1ac0bc1
edmante <dodgeandchase@gmail.com>
2023-08-29 15:42:45 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,12 +2,14 @@ module "network" {
   source  = "app.terraform.io/edmante/network/google"
   version = "3.4.0"
   # insert required variables here
+  project_id   = "p-rujlfpdyflq2-0"
+  network_name = "gaurav-network"
 
   subnets = [
   {
     subnet_name   = "gaurav-subnet"
     subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
+    subnet_region = "asia-east1"
   }
 ]
 }
