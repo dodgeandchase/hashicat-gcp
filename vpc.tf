768b59f68fef322b5a15e5344525ea7996448caf
edmante <dodgeandchase@gmail.com>
2023-08-29 15:37:58 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,45 +1,13 @@
-module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 3.0"
+module "network" {
+  source  = "app.terraform.io/edmante/network/google"
+  version = "7.3.0"
+  # insert required variables here
 
-    project_id   = "p-rujlfpdyflq2-0"
-    network_name = "gaurav-network"
-    routing_mode = "GLOBAL"
-
-    subnets = [
-        {
-            subnet_name           = "gaurav-subnet"
-            subnet_ip             = "10.100.10.0/24"
-            subnet_region         = "asia-east1"
-        }
-    ]
-#
-#    secondary_ranges = {
-#        edmante = [
-#            {
-#                range_name    = "subnet-01-secondary-01"
-#                ip_cidr_range = "192.168.64.0/24"
-#            },
-#        ]
-#
-#        subnet-02 = []
-#    }
-#
-#    routes = [
-#        {
-#            name                   = "egress-internet"
-#            description            = "route through IGW to access internet"
-#            destination_range      = "0.0.0.0/0"
-#            tags                   = "egress-inet"
-#            next_hop_internet      = "true"
-#        },
-#        {
-#            name                   = "app-proxy"
-#            description            = "route through proxy to reach app"
-#            destination_range      = "10.50.10.0/24"
-#            tags                   = "app-proxy"
-#            next_hop_instance      = "app-proxy-instance"
-#            next_hop_instance_zone = "us-west1-a"
-#        },
-#    ]
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
 }
